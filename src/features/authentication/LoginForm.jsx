baa3e9e3f3b2cb0dbd1cfd259ea6500d7b96d8dd
Paulo <info@monis-consulting.com>
2024-06-04 16:13:36 +0100

    adding preset testuser so people can try out the application easily

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("testuser@shelltech-solutions-llc.com");
+  const [password, setPassword] = useState("12345678");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
